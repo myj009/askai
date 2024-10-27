308edb0cd395f82f8e9cb884d2f0c26eae876ac3
myj009 <mohammad.jagora@deliveroo.com>
2024-10-27 12:30:26 +0530

    typewriter

--- src/components/launching-soon.tsx
+++ src/components/launching-soon.tsx
@@ -1,12 +1,23 @@
+"use client";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 import AnimatedGridPattern from "@/components/ui/animated-grid-pattern";
 import { cn } from "@/lib/utils";
 
 export function LaunchingSoon() {
+  const words = [
+    {
+      text: "Launching",
+    },
+    {
+      text: "Soon...",
+    },
+  ];
+
   return (
     <div className="relative flex h-screen w-full items-center justify-center overflow-hidden rounded-lg bg-background p-20 md:shadow-xl">
-      <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-foreground">
-        Launching Soon...
-      </p>
+      <div className="z-10">
+        <TypewriterEffectSmooth words={words} />
+      </div>
       <AnimatedGridPattern
         numSquares={30}
         maxOpacity={0.1}
